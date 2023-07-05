8e92de8252ea7450de0b1da7e6c0aedc4cef0b44
Aman Raj <94061474+amanraj2396@users.noreply.github.com>
2023-07-06 00:20:45 +0530

    Create lecture34-I.cpp

---
+++ Lecture34-I-CipherSchool/lecture34-I.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+using namespace std;
+
+class node
+{
+      public:
+
+      int data;
+      Node *next1;
+      Node *next2;
+      Node *next3;
+};
+
+int main();
+{
+    string CEO;
+
+    string PA,CFO;
+
+}
